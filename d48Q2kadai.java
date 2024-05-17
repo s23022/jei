2f05854243b5e96ac5fc36bb6aa09fc137617fa1
s23022 <s23022@std.it-college.ac.jp>
2024-05-17 11:50:13 +0900

    add kadai 0517

--- d48Q2kadai.java
+++ d48Q2kadai.java
@@ -1,3 +1,6 @@
+// s23022
+// ポリフィズムの課題
+
 // スーパークラスAnimalの定義
 class Animal {
 	public void makeSound() {
@@ -14,12 +17,20 @@ class Dog extends Animal {
 		System.out.println("ワンワン");
 	}
 }
+class Cat extends Animal {
+	public void makeSound() {
+		System.out.println("ニャー");
+	}
+}
 
 public class d48Q2kadai {
 	public static void main(String[] args) {
 		Animal a = new Dog(); //インスタンス化
+		Animal b = new Cat();
 		a.makeSound(); 
 		a.test();
+		b.makeSound();
+		b.test();
 	}
 }
 
