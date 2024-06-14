8fc57a4aeaca74e3775b0c20d8d6fc9521df23c6
s23022 <s23022@std.it-college.ac.jp>
2024-06-14 12:43:18 +0900

    修正

--- d48Q26kadai.java
+++ d48Q26kadai.java
@@ -1,10 +1,19 @@
 public class d48Q26kadai {
 	public static void main(String[] args) {
-		d48Q26kadai obj = new d48Q26kadai();
-		obj.show(5.5);
+		if (args.length > 0) {
+			try {
+				double inputValue = Double.parseDouble(args[0]);
+				d48Q26kadai obj = new d48Q26kadai();
+				obj.show(inputValue);
+			} catch (NumberFormatException e) {
+				System.out.println("Invalid input. Please provide a valid number.");
+			}
+		} else {
+			System.out.println("Please provide a number as a command line argument.");
+		}
 	}
 
 	void show(double n) {
-		System.out.print("value is" + n);
+		System.out.print("value is " + n);
 	}
 }
