0584fb565c5770bdd62cea86eebcda5a50c92528
s23022 <s23022@std.it-college.ac.jp>
2024-06-14 12:29:40 +0900

    0614 30page Q26

---
+++ d48Q26kadai.java
@@ -0,0 +1,10 @@
+public class d48Q26kadai {
+	public static void main(String[] args) {
+		d48Q26kadai obj = new d48Q26kadai();
+		obj.show(5.5);
+	}
+
+	void show(double n) {
+		System.out.print("value is" + n);
+	}
+}
